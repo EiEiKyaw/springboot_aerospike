e9cb52a5cdfda60bfa1b01e5f97e971328524745
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-08-17 13:28:58 +0630

    first commit

---
+++ src/test/java/org/akee/testing/springboot_aerospike/SpringbootAerospikeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akee.testing.springboot_aerospike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAerospikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
